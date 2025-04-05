d6841167502f134a086eec8cc75738190a04865f
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2025-04-06 00:41:33 +0200

    5-representing-code

---
+++ jLox/src/com/craftinginterpreters/lox/RpnPrinter.java
@@ -0,0 +1,53 @@
+package com.craftinginterpreters.lox;
+
+class RpnPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " +
+                expr.right.accept(this) + " " +
+                expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    public static void main(String[] args) {
+        // input: (1 + 2) * (4 - 3)
+        // expected: 1 2 + 4 3 - *
+        var expression = new Expr.Binary(
+                new Expr.Grouping(
+                        new Expr.Binary(
+                                new Expr.Literal(1),
+                                new Token(TokenType.PLUS, "+", null, 1), // Assuming Token constructor takes a lexeme
+                                new Expr.Literal(2)
+                        )
+                ),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Binary(
+                                new Expr.Literal(4),
+                                new Token(TokenType.MINUS, "-", null, 1), // Assuming Token constructor takes a lexeme
+                                new Expr.Literal(3)
+                        )
+                )
+        );
+
+        System.out.println(new RpnPrinter().print(expression));
+    }
+}
